b0b9f863fc9a7465836651378199e70878d4aa25
Milan Bugarski <bugarskimilan@gmail.com>
2020-08-09 22:49:11 +0200

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA73GTMlTIfs_37MBShoxrtbUrpfWzTUGE',
+    authDomain: 'ngt-test-76697.firebaseapp.com',
+    databaseURL: 'https://ngt-test-76697.firebaseio.com',
+    projectId: 'ngt-test-76697',
+    storageBucket: 'ngt-test-76697.appspot.com',
+    messagingSenderId: '668098799298',
+    appId: '1:668098799298:web:2bce7da51f4b9a01037d5b',
+    measurementId: 'G-CQGW4WMPRP',
+  },
 };
